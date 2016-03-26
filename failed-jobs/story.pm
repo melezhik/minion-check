0e76463aff44888417208c7e4488568e2f22c62e
Alexey Melezhik <melezhik@gmail.com>
2016-03-26 14:49:11 +0300

    check mninion

---
+++ failed-jobs/story.pm
@@ -0,0 +1,15 @@
+set_stdout(<<HERE) if $ENV{'dev_mod'};
+Inactive workers: 1
+Active workers:   0
+Inactive jobs:    0
+Active jobs:      0
+Failed jobs:      0
+Finished jobs:    0
+Inactive workers: 1
+Active workers:   0
+Inactive jobs:    0
+Active jobs:      0
+Failed jobs:      2
+Finished jobs:    0
+HERE
+
---
+++ jobs/story.pm
@@ -0,0 +1,4 @@
+set_stdout(<<HERE) if $ENV{dev_mod};
+67  inactive  sparrowhub.org:4616  2016-03-19T10:05:46Z
+67  inactive  sparrowhub.org:4616  2016-03-19T10:05:46Z
+HERE
