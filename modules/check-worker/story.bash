cbacc821d706ee1a63ed2b7565c8749cd04bb693
Alexey Melezhik <melezhik@gmail.com>
2017-03-02 13:09:04 +0000

    minor updates

--- modules/check-worker/story.bash
+++ modules/check-worker/story.bash
@@ -2,6 +2,6 @@ worker_cmd=$(config worker_cmd)
 
 echo look up "{{{$worker_cmd}}}" at proccess list ...
 
-ps uax | grep "$worker_cmd" | grep -v grep && echo done
+ps uax | grep "$worker_cmd" | grep -v grep || exit 2
 
 
